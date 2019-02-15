0b6c2787c448221ebff748bb93fed32b43758f69
mghalbasch <mghalbasch@ucdavis.edu>
2019-02-15 11:17:55 -0500

    Sped up the first assignment code a bit

--- R_Programming/R_Programming_Week2/complete.R
+++ R_Programming/R_Programming_Week2/complete.R
@@ -25,12 +25,15 @@ read_files <- function(directory, id = 1:332) {
   return(data)
 }
 
-complete <- function(directory, id=1:332) {
+complete <- function(directory, id=1:332, monitor_data = NULL) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
   
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
+  
+  ## 'monitor_data' is an optional argument proivding data if
+  ## we have it, so we don't have to read it in twice.
   
   ## Return a data frame of the form:
   ## id nobs
@@ -41,8 +44,12 @@ complete <- function(directory, id=1:332) {
   ## complete cases
   require(dplyr)
   
-  air_data <- read_files(directory, id)
-  air_data %>% filter(is.na(nitrate) == FALSE &
+  # First we read the data if we were not provided with it
+  if(is.null(monitor_data)){monitor_data <- read_files(directory, id)}
+  
+  # Next we check for complete cases (where both nitrate and sulfate
+  # are provided) and count the number of observations.
+  monitor_data %>% filter(is.na(nitrate) == FALSE &
                         is.na(sulfate) == FALSE) %>%
     group_by(ID) %>% summarize(nobs = n()) %>% as.data.frame()
 }
--- R_Programming/R_Programming_Week2/corr.R
+++ R_Programming/R_Programming_Week2/corr.R
@@ -1,4 +1,4 @@
-corr <- function(directory, threshold = 0) {
+corr <- function(directory, threshold = 0, monitor_data = NULL) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
   
@@ -7,16 +7,20 @@ corr <- function(directory, threshold = 0) {
   ## required to compute the correlation between nitrate and sulfate;
   ## the default is 0
   
+  ## 'monitor_data' is an optional argument that provides data, so that
+  ## we don't have to read the data in twice
+  
   ## Return a numeric vector of correlations
   ## NOTE: Do not round the result!
   
   require(dplyr)
   
-  air_data <- read_files(directory)
-  nobs_df <- complete(directory)
+  # First get the data if we weren't provided with it
+  if(is.null(monitor_data)){monitor_data <- read_files(directory)}
+  nobs_df <- complete(monitor_data = monitor_data)
   nobs_df %>% filter(nobs > threshold) %>% select(ID) -> good_ids
   
-  air_data %>% filter(ID %in% good_ids$ID) %>%
+  monitor_data %>% filter(ID %in% good_ids$ID) %>%
     group_by(ID) %>%
     summarise(correlations = cor(sulfate, 
                                  nitrate, use = "complete.obs")) %>%
